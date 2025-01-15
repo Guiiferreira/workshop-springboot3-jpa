4402172e24f1289cc2d54780514972d670f048d0
Guilherme Ferreira <guilhermepereiraferreira4@gmail.com>
2025-01-15 18:33:32 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -48,8 +48,19 @@ public class TestConfig implements CommandLineRunner {
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		//associando produtos com as categorias   
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		User u1 = new User(null, "Gui Ferreira", "gui@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Marta pereira", "marta@gmail.com", "977777777", "123456");
